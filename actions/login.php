06df15476ff2e4e38505f442ef11cd8974c5783b
BuildTools <unconfigured@null.spigotmc.org>
2018-02-08 16:19:23 -0600

    Port old username and password Schem site to using username and tokens from DB

--- actions/login.php
+++ actions/login.php
@@ -1,10 +1,10 @@
 <?php
 
-if (empty($_POST['username']) || empty($_POST['password']))
-	fail("You need to submit a username and password.");
+if (empty($_POST['username']) || empty($_POST['token']))
+	fail("You need to submit a username and token.");
 
 $username = $_POST['username'];
-$password = $_POST['password'];
+$token = $_POST['token'];
 
 $db = new Mysqli(
 	$conf->db->host,
@@ -19,29 +19,32 @@ if ($db->connect_error)
 //We don't want SQL injection.
 $username = $db->real_escape_string($username);
 
-$res = $db->query("SELECT * FROM users WHERE username='$username'");
+
+$uuid = getUUID($username);
+
+$res = $db->query("SELECT EXISTS(SELECT * FROM tokens WHERE uuid='$uuid')");
 
 $db->close();
 
 if (empty($res))
 	fail("User $username doesn't exist.");
 
-$user = $res->fetch_assoc();
-$hash = MD5($user['salt'] . $_POST['password']);
 
-if ($hash === $user['password'])
+$user = $res->fetch_assoc();
+
+if ($token === $user['token'])
 {
 	$_SESSION['loggedin'] = true;
 	$_SESSION['username'] = $username;
-	$_SESSION['uuid'] = getUUID($username);
+	$_SESSION['uuid'] = $uuid;
 
 	//Create the user's directory if it doesn't exist
-	if (!file_exists("$conf->schemsDir/$username"))
-		mkdir("$conf->schemsDir/$username");
+	if (!file_exists("$conf->schemsDir/$uuid"))
+		mkdir("$conf->schemsDir/$uuid");
 
 	redirect("browse");
 }
 else
 {
-	fail("Wrong username or password.");
+	fail("Wrong username or token.");
 }
--- views/login/index.php
+++ views/login/index.php
@@ -8,7 +8,7 @@
 		template("map",
 		[
 			"Username"=>"<input class='textfield' type='text' id='username' name='username'>",
-			"Password"=>"<input class='textfield' type='password' id='password' name='password'>"
+			"Token"=>"<input class='textfield' type='password' id='token' name='token'>"
 		])
 	?>
 	<button class='navbutton'>Log In</button>
